242bfebc672f15ffb0c37bf5a73f2fbfbeaea606
wwylvswy <wuweeye@163.com>
2025-12-06 13:07:28 +0800

    update env info

--- src/consts.ts
+++ src/consts.ts
@@ -7,8 +7,8 @@ export const CARD_SPACING_SM = 24
  * GitHub 仓库配置
  */
 export const GITHUB_CONFIG = {
-	OWNER: process.env.NEXT_PUBLIC_GITHUB_OWNER || 'yysuni',
-	REPO: process.env.NEXT_PUBLIC_GITHUB_REPO || '2025-blog-public',
+	OWNER: process.env.NEXT_PUBLIC_GITHUB_OWNER || 'wwylvswy',
+	REPO: process.env.NEXT_PUBLIC_GITHUB_REPO || 'hakimi-blog',
 	BRANCH: process.env.NEXT_PUBLIC_GITHUB_BRANCH || 'main',
 	APP_ID: process.env.NEXT_PUBLIC_GITHUB_APP_ID || '-'
 } as const
