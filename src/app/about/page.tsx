9057cf9bff9a71ef77ef31be4479a56622bab5d5
wwylvswy <wuweeye@163.com>
2025-12-08 22:02:35 +0800

    update env about's github link.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -151,7 +151,7 @@ export default function Page() {
 
 					<div className='mt-8 flex items-center justify-center gap-6'>
 						<motion.a
-							href='https://github.com/YYsuni/2025-blog-public'
+							href='https://github.com/wwylvswy/hakimi-blog'
 							target='_blank'
 							rel='noreferrer'
 							initial={{ opacity: 0, scale: 0.6 }}
